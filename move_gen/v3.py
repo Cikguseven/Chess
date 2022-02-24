4c24254b63767c4c377d6b54f09833b0ab7844bd
Kieron Seven Lee <40373653+Cikguseven@users.noreply.github.com>
2022-02-24 17:11:43 +0800

    updates

--- move_gen/v3.py
+++ move_gen/v3.py
@@ -3,7 +3,80 @@ import time
 start = time.time()
 
 
-def main():
+def main(raw_fen, depth):
+
+    knight_moves = [[10, 17], [11, 16, 18], [8, 12, 17, 19], [9, 13, 18, 20],
+                    [10, 14, 19, 21], [11, 15, 20, 22], [12, 21, 23], [13, 22],
+                    [2, 18, 25], [3, 19, 24, 26], [0, 4, 16, 20, 25, 27],
+                    [1, 5, 17, 21, 26, 28], [2, 6, 18, 22, 27, 29],
+                    [3, 7, 19, 23, 28, 30], [4, 20, 29, 31], [5, 21, 30],
+                    [1, 10, 26, 33], [0, 2, 11, 27, 32, 34],
+                    [1, 3, 8, 12, 24, 28, 33, 35],
+                    [2, 4, 9, 13, 25, 29, 34, 36],
+                    [3, 5, 10, 14, 26, 30, 35, 37],
+                    [4, 6, 11, 15, 27, 31, 36, 38],
+                    [5, 7, 12, 28, 37, 39], [6, 13, 29, 38], [9, 18, 34, 41],
+                    [8, 10, 19, 35, 40, 42], [9, 11, 16, 20, 32, 36, 41, 43],
+                    [10, 12, 17, 21, 33, 37, 42, 44],
+                    [11, 13, 18, 22, 34, 38, 43, 45],
+                    [12, 14, 19, 23, 35, 39, 44, 46], [13, 15, 20, 36, 45, 47],
+                    [14, 21, 37, 46], [17, 26, 42, 49],
+                    [16, 18, 27, 43, 48, 50], [17, 19, 24, 28, 40, 44, 49, 51],
+                    [18, 20, 25, 29, 41, 45, 50, 52],
+                    [19, 21, 26, 30, 42, 46, 51, 53],
+                    [20, 22, 27, 31, 43, 47, 52, 54], [21, 23, 28, 44, 53, 55],
+                    [22, 29, 45, 54], [25, 34, 50, 57],
+                    [24, 26, 35, 51, 56, 58], [25, 27, 32, 36, 48, 52, 57, 59],
+                    [26, 28, 33, 37, 49, 53, 58, 60],
+                    [27, 29, 34, 38, 50, 54, 59, 61],
+                    [28, 30, 35, 39, 51, 55, 60, 62], [29, 31, 36, 52, 61, 63],
+                    [30, 37, 53, 62], [33, 42, 58], [32, 34, 43, 59],
+                    [33, 35, 40, 44, 56, 60], [34, 36, 41, 45, 57, 61],
+                    [35, 37, 42, 46, 58, 62], [36, 38, 43, 47, 59, 63],
+                    [37, 39, 44, 60], [38, 45, 61], [41, 50], [40, 42, 51],
+                    [41, 43, 48, 52], [42, 44, 49, 53], [43, 45, 50, 54],
+                    [44, 46, 51, 55], [45, 47, 52], [46, 53]]
+
+    king_moves = [[1, 8, 9], [2, 8, 9, 10, 0], [3, 9, 10, 11, 1],
+                  [4, 10, 11, 12, 2], [5, 11, 12, 13, 3], [6, 12, 13, 14, 4],
+                  [7, 13, 14, 15, 5], [14, 15, 6], [9, 16, 17, 0, 1],
+                  [10, 16, 17, 18, 0, 1, 2, 8], [11, 17, 18, 19, 1, 2, 3, 9],
+                  [12, 18, 19, 20, 2, 3, 4, 10], [13, 19, 20, 21, 3, 4, 5, 11],
+                  [14, 20, 21, 22, 4, 5, 6, 12], [15, 21, 22, 23, 5, 6, 7, 13],
+                  [22, 23, 6, 7, 14], [17, 24, 25, 8, 9],
+                  [18, 24, 25, 26, 8, 9, 10, 16],
+                  [19, 25, 26, 27, 9, 10, 11, 17],
+                  [20, 26, 27, 28, 10, 11, 12, 18],
+                  [21, 27, 28, 29, 11, 12, 13, 19],
+                  [22, 28, 29, 30, 12, 13, 14, 20],
+                  [23, 29, 30, 31, 13, 14, 15, 21], [30, 31, 14, 15, 22],
+                  [25, 32, 33, 16, 17], [26, 32, 33, 34, 16, 17, 18, 24],
+                  [27, 33, 34, 35, 17, 18, 19, 25],
+                  [28, 34, 35, 36, 18, 19, 20, 26],
+                  [29, 35, 36, 37, 19, 20, 21, 27],
+                  [30, 36, 37, 38, 20, 21, 22, 28],
+                  [31, 37, 38, 39, 21, 22, 23, 29], [38, 39, 22, 23, 30],
+                  [33, 40, 41, 24, 25], [34, 40, 41, 42, 24, 25, 26, 32],
+                  [35, 41, 42, 43, 25, 26, 27, 33],
+                  [36, 42, 43, 44, 26, 27, 28, 34],
+                  [37, 43, 44, 45, 27, 28, 29, 35],
+                  [38, 44, 45, 46, 28, 29, 30, 36],
+                  [39, 45, 46, 47, 29, 30, 31, 37], [46, 47, 30, 31, 38],
+                  [41, 48, 49, 32, 33], [42, 48, 49, 50, 32, 33, 34, 40],
+                  [43, 49, 50, 51, 33, 34, 35, 41],
+                  [44, 50, 51, 52, 34, 35, 36, 42],
+                  [45, 51, 52, 53, 35, 36, 37, 43],
+                  [46, 52, 53, 54, 36, 37, 38, 44],
+                  [47, 53, 54, 55, 37, 38, 39, 45], [54, 55, 38, 39, 46],
+                  [49, 56, 57, 40, 41], [50, 56, 57, 58, 40, 41, 42, 48],
+                  [51, 57, 58, 59, 41, 42, 43, 49],
+                  [52, 58, 59, 60, 42, 43, 44, 50],
+                  [53, 59, 60, 61, 43, 44, 45, 51],
+                  [54, 60, 61, 62, 44, 45, 46, 52],
+                  [55, 61, 62, 63, 45, 46, 47, 53], [62, 63, 46, 47, 54],
+                  [57, 48, 49], [58, 48, 49, 50, 56], [59, 49, 50, 51, 57],
+                  [60, 50, 51, 52, 58], [61, 51, 52, 53, 59],
+                  [62, 52, 53, 54, 60], [63, 53, 54, 55, 61], [54, 55, 62]]
 
     # Returns algebraic coordinates of start and final position
     def an(i1, i2):
@@ -85,51 +158,35 @@ def main():
 
         # Knight moves
         elif piece_id == 'n':
-            for target in [-17, -15, -10, -6, 6, 10, 15, 17]:
-                move = index + target
+            moves = knight_moves[index]
 
-                if move > 63:
-                    break
+            # Inclues all squares in enemy's attack path
+            if flag > 0:
+                return moves, None
 
-                # Sum of rows and columns of old and new knight pos must be odd
-                elif (move >= 0 and (row + col + move // 8 + move % 8) % 2
-                        and (flag > 0 or lzero(move) not in tpp)):
-                    piece_moves.append(move)
-
-            return piece_moves, None
+            return [i for i in moves if lzero(i) not in tpp], None
 
         # King moves
         elif piece_id == 'k':
-            targets = {-9, -8, -7, -1, 1, 7, 8, 9}
-
-            if col == 0:
-                targets -= {-9, -1, 7}
-            elif col == 7:
-                targets -= {-7, 1, 9}
-            if row == 0:
-                targets -= {-9, -8, -7}
-            elif row == 7:
-                targets -= {7, 8, 9}
+            moves = king_moves[index]
 
             # Inclues all adjacent squares in enemy's attack path
             if flag > 0:
-                piece_moves = [index + i for i in targets]
+                return moves, None
 
-            else:
-                piece_moves = [
-                    index + i for i in targets if lzero(index + i) not in tpp]
+            piece_moves = [i for i in moves if lzero(i) not in tpp]
 
             # Generates castling moves if available
-                if col == 4 and not row % 7:
-                    left = [index - 1, index - 2, index - 3]
-                    right = [index + 1, index + 2]
-                    app = epp + tpp
+            if col == 4 and not row % 7:
+                left = [index - 1, index - 2, index - 3]
+                right = [index + 1, index + 2]
+                app = epp + tpp
 
-                    if not any(lzero(i) in app for i in left):
-                        piece_moves.append(index - 2)
+                if not any(lzero(i) in app for i in left):
+                    piece_moves.append(index - 2)
 
-                    if not any(lzero(i) in app for i in right):
-                        piece_moves.append(index + 2)
+                if not any(lzero(i) in app for i in right):
+                    piece_moves.append(index + 2)
 
             return piece_moves, None
 
@@ -361,11 +418,20 @@ def main():
 
                     # Stops move generation if depth = 1
                     if depth == 1:
-                        counter += 1
 
                         # 4 possible pieces to promote to
                         if (piece_id == 'p' and not (np // 8) % 7):
-                            counter += 3
+                            counter += 4
+                            if depth == ctd:
+                                print(f'{an(index, np)}q: 1')
+                                print(f'{an(index, np)}r: 1')
+                                print(f'{an(index, np)}b: 1')
+                                print(f'{an(index, np)}n: 1')
+
+                        else:
+                            counter += 1
+                            if depth == ctd:
+                                print(f'{an(index, np)}: 1')
 
                     else:
                         cstl_copy = cstl
@@ -446,7 +512,8 @@ def main():
                                                    wpp_copy, bpp_copy)
 
                                 if depth == ctd:
-                                    print(f'{an(index, np)}{i}: {r_depth}')
+                                    print(f'{an(index, np)}{i.lower()}: '
+                                          f'{r_depth}')
 
                                 counter += r_depth
                         # Continues with recursive move generation
@@ -462,10 +529,7 @@ def main():
                             counter += r_depth
         return counter
 
-    # Input FEN of position
-    test = 'rnb1kb1r/pp1p2pp/2p5/q7/8/3P4/PPP1PPPP/RN2KBNR w KQkq - 0 1'
-
-    fen_info = process_fen(test)
+    fen_info = process_fen(raw_fen)
     turn = fen_info['turn']
     pm = fen_info['en_passant']
     bkp = fen_info['bkp']
@@ -473,32 +537,40 @@ def main():
     bpp = fen_info['black_piece_pos']
     wpp = fen_info['white_piece_pos']
     cstl = fen_info['castling']
-    depth = 5
     check_to_depth = depth
     print(f'go perft {depth}')
 
-    print(move_gen(depth, check_to_depth, turn, pm, cstl, wkp, bkp, wpp, bpp))
+    return move_gen(depth, check_to_depth, turn, pm, cstl, wkp, bkp, wpp, bpp)
 
 
 if __name__ == '__main__':
-    main()
+
+    # For testing single position
+    # raw_fen = '4kN1r/Pp2n2p/8/1K1P2r1/4P3/6P1/RP2p2P/2n5 b k - 0 6'
+    # depth = 1
+    # print(main(raw_fen, depth))
 
     # For testing multiple positions
-    # positions = open('perft_benchmark.txt', 'r').read().splitlines()
-    # for p in positions:
-    #     if p[0] != '#':
-    #         depth = int(p[0])
-    #         fen_start_index = p.rfind(',')
-    #         raw_fen = p[fen_start_index + 1:]
-    #         start = time.time()
-    #         result = main(raw_fen, depth)
-    #         end = time.time()
-    #         duration = end - start
-    #         print(result)
-    #         print(duration)
-    #         print(result / duration)
-    #         print()
-    #         print()
+    positions = open('perft_suite.txt', 'r').read().splitlines()
+    for p in positions:
+        if p[0] != '#':
+            depth = int(p[0])
+            fen_start_index = p.rfind(',')
+            raw_fen = p[fen_start_index + 1:]
+            start = time.time()
+            result = main(raw_fen, depth)
+            end = time.time()
+            duration = end - start
+            print(result)
+            correct_result = int(p[p.find(',') + 1:fen_start_index])
+            if result == correct_result:
+                print('correct')
+            else:
+                print('wrong')
+            print(f'time taken: {duration}')
+            print(f'nps: {result / 1}')
+            print()
+            print()
 
 end = time.time()
 
--- test.py
+++ test.py
@@ -1,9 +1,40 @@
-def x(n):
-    if n == 0:
-        return 0
-    else:
-        return x(n // 2) + (n % 2)
+from pprint import *
 
+x = [-9, -8, -7, -1, 1, 7, 8, 9]
 
-for i in range(1, 17):
-    print(x(i))
+result = []
+
+for i in range(64):
+    row = i // 8
+    col = i % 8
+    targets = {-9, -8, -7, -1, 1, 7, 8, 9}
+    if col == 0:
+        targets -= {-9, -1, 7}
+    elif col == 7:
+        targets -= {-7, 1, 9}
+    if row == 0:
+        targets -= {-9, -8, -7}
+    elif row == 7:
+        targets -= {7, 8, 9}
+    result.append([i + j for j in targets])
+
+print(result)
+
+# counter = 0
+# for i in result:
+#     print(counter)
+#     for rank in range(8):
+#         for file in range(8):
+#             if file == 0:
+#                 print()
+#                 print(f"{8 - rank}    ", end="")
+#             if rank * 8 + file in i:
+#                 print("1 ", end="")
+#             else:
+#                 print(". ", end="")
+#     print()
+#     print()
+#     print("     a b c d e f g h")
+#     print()
+#     print()
+#     counter += 1
